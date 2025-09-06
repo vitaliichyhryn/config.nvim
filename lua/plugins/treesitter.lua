2fb1009163ca36a166a3bb2664d70c3cc2786e5c
Vitalii Chyhryn <chyhryn.vitalii@gmail.com>
2025-09-06 22:43:01 +0300

    Revert to master branch for nvim.treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 vim.pack.add({
-    { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
+    {
+        src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+        version = 'master'
+    },
     { src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects' },
 })
 
