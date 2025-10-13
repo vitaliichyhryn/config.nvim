65e55e6e032d6b2507568c31c9257d1484ab023b
Vitalii Chyhryn <chyhryn.vitalii@gmail.com>
2025-10-13 16:45:08 +0300

    Enable Prettier for Markdown

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,6 +2,7 @@ vim.pack.add({ { src = 'https://github.com/stevearc/conform.nvim' } })
 
 require('conform').setup({
     formatters_by_ft = {
+        markdown = { 'prettier' },
         lua = { 'stylua' },
     },
     format_on_save = {
